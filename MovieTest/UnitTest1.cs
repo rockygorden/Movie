d78cda16caa1a7467e0333bb6025c6078ebdd868
My GitHub Project <naveenkumarsingh@live.com>
2015-05-29 15:46:43 +0530

    added testproject

---
+++ MovieTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int sum = 1 + 4;
+            Assert.AreEqual(3, sum);
+        }
+    }
+}
